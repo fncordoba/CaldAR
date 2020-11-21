7a3ab14d422e03495fa504d2b55387522b1274b3
Valentín Silvestri <valentinsilvestri@outlook.com>
2020-11-21 17:23:33 -0300

    feature-13: Added changes on style

--- src/controllers/techniciansController.js
+++ src/controllers/techniciansController.js
@@ -1,9 +1,9 @@
 const technicians = require ("../data/technicians.json");
+
 const getAllTechnicians = () => {
     return technicians;
 };
 
-// getAllTechnicians
 module.exports = {
     getAllTechnicians
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const techniciansController = require('./controllers/techniciansController');
+
 const app = express();
 const port = 3000;
 
-// getAllTechnicians
-app.get("/getAllTechnicians", (req, res)=>{
+app.get("/getAllTechnicians", (req, res) => {
   const technicians = techniciansController.getAllTechnicians();
   res.json(technicians);
 });
 
-// Server connection
 app.listen(port, () => {
   console.log(`CaldAR app listening at http://localhost:${port}`);
 });
