8360f0444bec6f0bcfc0b647e464c52ee0e4d981
Valentín Silvestri <valentinsilvestri@outlook.com>
2020-11-27 18:57:41 -0300

    feature-33: Add mongoose

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-console */
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./routes');
 const techniciansController = require('./controllers/techniciansController');
 const boylerTypesController = require('./controllers/boylerTypesController');
 const companiesController = require('./controllers/companiesController');
 const boylersController = require('./controllers/boylersController');
 const buildingsController = require('./controllers/buildingsController');
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.get('/getAllTechnicians', (req, res) => {
   const technicians = techniciansController.getAllTechnicians();
@@ -140,6 +149,14 @@ app.get('/removeBoylerById/:id', (req, res) => {
   res.json({
     message: result,
   });
+});
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log('Connected to the database.');
+}).catch((error) => {
+  console.log('Cannot connect to the database.', error);
 });
 
 app.listen(port, () => {
