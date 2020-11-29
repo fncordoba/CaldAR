abc4415c932d77fd17ae2ae4c42142fea45d8ae4
jose blois <joseblois3@gmail.com>
2020-11-29 14:15:52 -0300

    Solved requested changes

--- src/models/buildings.js
+++ src/models/buildings.js
@@ -1,28 +1,30 @@
-module.exports = mongoose => {
-  const Building = mongoose.model(
-    'buildings',
-    mongoose.Schema({
-      name: {
-        type: String,
-        required: true
-      },
-      adress: {
-        type: String,
-        required: true
-      },
-      company: {
-        type: String
-      },
-      phone: {
-        type: String,
-        required: true
-      },
-      boilers: [{
-        type: Number,
-        required: true
-      }]
+const mongoose = require('mongoose');
+
+const buildingSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
     },
-    { timestamps: true })
-  );
-  return Building;
-};
+    adress: {
+      type: String,
+      required: true
+    },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'companies',
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    boilers: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'boilers',
+      required: true,
+    }]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('buildings', buildingSchema);
