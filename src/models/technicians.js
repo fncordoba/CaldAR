0ba562c4f11019814eb7d79cdfe4c81be4f2a8da
Pablo Bloise <pabloise10@gmail.com>
2020-11-29 16:03:15 -0300

    Fixed eslint problems

--- src/models/technicians.js
+++ src/models/technicians.js
@@ -1,48 +1,47 @@
 const mongoose = require('mongoose');
 
 const techniciansSchema = mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: Number,
-        required: true
-    },
-    boilerTypes:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'boilerTypes',
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    dateOfBirth: {
-        type: String,
-        required: true
-    },
-    monthlyCapacity: {
-        type: String,
-        required: true
-    },
-    hourRate: {
-        type: String,
-        required: true
-    }
+  firstName: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: Number,
+    required: true
+  },
+  boilerTypes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'boilerTypes',
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: String,
+    required: true
+  },
+  monthlyCapacity: {
+    type: String,
+    required: true
+  },
+  hourRate: {
+    type: String,
+    required: true
+  }
 },
 
-    {
-        timestamps: true
-    }
-);
+{
+  timestamps: true
+});
 
 module.exports = mongoose.model('technicians', techniciansSchema);
