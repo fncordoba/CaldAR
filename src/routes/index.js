3d0f80db17d5054f2536967e86cc824df32631ac
jose blois <joseblois3@gmail.com>
2020-11-29 17:00:39 -0300

    feature-44: Create a buildings router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const boilersRoutes = require('./boilersRoutes');
+const buildingsRoutes = require('./buildingsRoutes');
 
 const router = express.Router();
 
 router.use('/boilers', boilersRoutes);
+router.use('/buildings', buildingsRoutes);
 
 module.exports = router;
