5d90b5424e720baf3ad21ac9239323ed15f23084
Pablo Bloise <pabloise10@gmail.com>
2020-11-30 15:57:59 -0300

    Feature-58: findAllTechnicians

--- src/controllers/techniciansController.js
+++ src/controllers/techniciansController.js
@@ -1,37 +1,16 @@
-const fs = require('fs');
-const technicians = require('../data/technicians.json');
+const models = require('../models');
 
-const getAllTechnicians = () => technicians;
-
-const getTechniciansById = id => {
-  const technician = technicians.find(technicianItem => technicianItem.id.toString() === id);
-  return technician;
-};
-
-const getTechniciansBy = (name, lastName) => {
-  const techniciansByNameAndLastName = technicians.filter(
-    technician => technician.name === name && technician.lastName === lastName,
-  );
-  return techniciansByNameAndLastName;
-};
-
-const removeTechniciansBy = id => {
-  const search = technicians.find(technician => technician.id.toString() === id);
-  const updatedTechnicians = technicians.filter(technician => technician.id.toString() !== id);
-  if (search) {
-    fs.writeFileSync(
-      `${__dirname}/../data/technicians.json`,
-      JSON.stringify(updatedTechnicians),
-      { encoding: 'utf8', flag: 'w' },
-    );
-    return `Technician with id ${id} deleted`;
+const getAllTechnicians = async (req, res) => {
+  try {
+    const techniciansFromDB = await models.technicians.find({});
+    res.status(200).json(techniciansFromDB);
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Error: cannot find all the technicians'
+    });
   }
-  return `Technician with id ${id} not found.`;
 };
 
 module.exports = {
-  getAllTechnicians,
-  getTechniciansById,
-  getTechniciansBy,
-  removeTechniciansBy,
+  getAllTechnicians
 };
