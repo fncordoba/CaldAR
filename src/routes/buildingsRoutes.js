67df02caab7c60648d15982e57c8ba208f820eca
jose blois <joseblois3@gmail.com>
2020-12-05 17:45:54 -0300

    feature-77: added middlewares to the buildings routes

--- src/routes/buildingsRoutes.js
+++ src/routes/buildingsRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const buildingController = require('../controllers/buildingsController');
+const { buildingSchema } = require('../utils/validationSchemas');
+const { validateBody, validateParams } = require('../utils/validations');
 
 const router = express.Router();
 
 router
   .get('/', buildingController.getAllBuildings)
-  .get('/:id', buildingController.getBuildingById)
-  .post('/', buildingController.createBuilding)
-  .put('/:id', buildingController.updateBuilding)
-  .delete('/:id', buildingController.deleteBuilding);
+  .get('/:id', validateParams(buildingSchema, 'id'), buildingController.getBuildingById)
+  .post('/', validateBody(buildingSchema), buildingController.createBuilding)
+  .put('/:id', validateParams(buildingSchema, 'id'), buildingController.updateBuilding)
+  .delete('/:id', validateParams(buildingSchema, 'id'), buildingController.deleteBuilding);
 
 module.exports = router;
