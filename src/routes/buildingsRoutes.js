71d90522ba2fa705a8f839e3a72e6d6820209e5b
jose blois <joseblois3@gmail.com>
2020-12-06 00:12:31 -0300

    feature-77: created an id schema an fixed validateParams function

--- src/routes/buildingsRoutes.js
+++ src/routes/buildingsRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const buildingController = require('../controllers/buildingsController');
-const { buildingSchema } = require('../utils/validationSchemas');
+const schemas = require('../utils/validationSchemas');
 const { validateBody, validateParams } = require('../utils/validations');
 
 const router = express.Router();
 
 router
   .get('/', buildingController.getAllBuildings)
-  .get('/:id', validateParams(buildingSchema, 'id'), buildingController.getBuildingById)
-  .post('/', validateBody(buildingSchema), buildingController.createBuilding)
-  .put('/:id', validateParams(buildingSchema, 'id'), buildingController.updateBuilding)
-  .delete('/:id', validateParams(buildingSchema, 'id'), buildingController.deleteBuilding);
+  .get('/:id', validateParams(schemas.idSchema, 'id'), buildingController.getBuildingById)
+  .post('/', validateBody(schemas.buildingSchema), buildingController.createBuilding)
+  .put('/:id', validateParams(schemas.idSchema, 'id'), buildingController.updateBuilding)
+  .delete('/:id', validateParams(schemas.idSchema, 'id'), buildingController.deleteBuilding);
 
 module.exports = router;
