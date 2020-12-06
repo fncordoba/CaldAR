e2db8c2d90e9e5c1531c80af1a92f408ecf092d7
Mauricio Puccinelli <puccinellimauricio@gmail.com>
2020-12-06 15:46:56 -0300

    Added body and params validation functions

--- src/routes/companiesRoutes.js
+++ src/routes/companiesRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const companiesController = require('../controllers/companiesController');
+const { validateBody, validateParam } = require('../utils/validations');
+const schemas = require('../utils/validationSchemas');
 
 const router = express.Router();
 
 router
-  .get('/', companiesController.getAllCompanies)
-  .get('/:id', companiesController.getCompanyById)
-  .post('/', companiesController.createCompany)
-  .put('/:id', companiesController.updateCompany)
-  .delete('/:id', companiesController.deleteCompany);
+  .get('/', validateBody(schemas.companiesSchema), validateParam(schemas.companiesSchema), companiesController.getAllCompanies)
+  .get('/:id', validateBody(schemas.companiesSchema), validateParam(schemas.companiesSchema), companiesController.getCompanyById)
+  .post('/', validateBody(schemas.companiesSchema), validateParam(schemas.companiesSchema), companiesController.createCompany)
+  .put('/:id', validateBody(schemas.companiesSchema), validateParam(schemas.companiesSchema), companiesController.updateCompany)
+  .delete('/:id', validateBody(schemas.companiesSchema), validateParam(schemas.companiesSchema), companiesController.deleteCompany);
 
 module.exports = router;
