95d60f3ef389d60ed51827cc0adf3df294b20bef
Valentín Silvestri <valentinsilvestri@outlook.com>
2020-12-07 13:03:45 -0300

    if-else statements added inside try-catch.

--- src/controllers/boilersController.js
+++ src/controllers/boilersController.js
@@ -7,7 +7,7 @@ const getAllBoilers = async (req, res) => {
     return res.status(200).json(boilers);
   } catch (error) {
     return res.status(500).json({
-      msg: 'An error has occurred',
+      msg: 'An error has occurred.',
     });
   }
 };
@@ -18,34 +18,35 @@ const getBoilerById = async (req, res) => {
 
     if (!boiler) {
       return res.status(400).json({
-        msg: 'The boiler has not been found',
+        msg: 'The boiler has not been found.',
       });
     }
     return res.status(200).json(boiler);
   } catch (error) {
     return res.status(500).json({
-      msg: 'An error has occurred',
+      msg: 'An error has occurred.',
     });
   }
 };
 
 const createBoiler = async (req, res) => {
-  const boilerType = await models.BoilerTypes.findById(req.body.boilerType);
-
-  if (!boilerType) {
-    return res.status(400).json({
-      msg: 'The boiler type does not exist in the DataBase.',
-    });
-  }
-  const newBoiler = new models.Boilers({
-    description: req.body.description,
-    boilerType: req.body.boilerType,
-    hourMaintenanceCost: req.body.hourMaintenanceCost,
-    hourEventualCost: req.body.hourEventualCost,
-    maintenanceRate: req.body.maintenanceRate,
-  });
   try {
+    const boilerType = await models.BoilerTypes.findById(req.body.boilerType);
+
+    if (!boilerType) {
+      return res.status(400).json({
+        msg: 'The boiler type does not exist in the DataBase.',
+      });
+    }
+    const newBoiler = new models.Boilers({
+      description: req.body.description,
+      boilerType: req.body.boilerType,
+      hourMaintenanceCost: req.body.hourMaintenanceCost,
+      hourEventualCost: req.body.hourEventualCost,
+      maintenanceRate: req.body.maintenanceRate,
+    });
     const result = await newBoiler.save();
+
     return res.status(200).json(result);
   } catch (error) {
     return res.status(500).json({
@@ -55,15 +56,16 @@ const createBoiler = async (req, res) => {
 };
 
 const updateBoiler = async (req, res) => {
-  const boilerType = await models.BoilerTypes.findById(req.body.boilerType);
-
-  if (!boilerType) {
-    return res.status(400).json({
-      msg: 'The boiler type does not exist in the DataBase.',
-    });
-  }
   try {
+    const boilerType = await models.BoilerTypes.findById(req.body.boilerType);
+
+    if (!boilerType) {
+      return res.status(400).json({
+        msg: 'The boiler type does not exist in the DataBase.',
+      });
+    }
     const result = await models.Boilers.findByIdAndUpdate(req.params.id, req.body, { new: true, });
+
     if (!result) {
       return res.status(400).json({
         msg: 'The boiler has not been found.',
