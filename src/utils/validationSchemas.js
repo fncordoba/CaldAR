5a83cf579f659d331de90424ac7cea2a88a34bcb
Mauricio Puccinelli <puccinellimauricio@gmail.com>
2020-12-07 12:46:17 -0300

    Changes requested

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -5,22 +5,18 @@ const companiesSchema = Joi.object({
     .min(3)
     .max(30)
     .required(),
-
   address: Joi.string()
     .min(3)
     .max(30)
     .required(),
-
   cuit: Joi.string()
     .alphanum()
     .max(11)
     .required(),
-
   phone: Joi.string()
     .min(9)
     .max(18)
     .required(),
-
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
     .required(),
